85cb0ee3fc713f815404c6982b3a45c46b1e0c0f
Alvaro Guzman <alvaro.guzman@ctc.edu.do>
2021-09-28 17:41:07 -0400

    first commit

---
+++ Final-Proyect/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Final_Proyect.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
